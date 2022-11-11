be40438f3ba7d87022852bdd49feb69ea119ecd2
Barbara <basia@tiki.mimuw.edu.pl>
2022-11-11 21:11:37 +0000

    wget->curl

--- example.sh
+++ example.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://mimuw.edu.pl/~bp209493/phasedancer_example.tar.gz
+curl https://mimuw.edu.pl/~bp209493/phasedancer_example.tar.gz -o phasedancer_example.tar.gz
 tar -xvzf phasedancer_example.tar.gz
-docker run -d -v `pwd`/phasedancer_example:/phaseDancerData -it --name phasedancer phasedancer:1.0
+docker run -d -v `pwd`/phasedancer_example:/phaseDancerData -it --name phasedancer bposzewiecka/phasedancer:1.0
 docker exec phasedancer ./run_phaseDancer.sh flat2 all 1 10
 docker exec phasedancer ./unload_index.sh flat2 1
 docker stop phasedancer
