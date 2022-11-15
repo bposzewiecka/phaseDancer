513b1d60b985080f7693609d3d753a272eb7d937
Barbara <basia@tiki.mimuw.edu.pl>
2022-11-15 19:24:15 +0000

    example changed

--- example.sh
+++ example.sh
@@ -3,8 +3,8 @@
 curl https://mimuw.edu.pl/~bp209493/phasedancer_example.tar.gz -o phasedancer_example.tar.gz
 tar -xvzf phasedancer_example.tar.gz
 docker run -d -v `pwd`/phasedancer_example:/phaseDancerData -it --name phasedancer bposzewiecka/phasedancer:1.0
-docker exec phasedancer ./run_phaseDancer.sh flat2 all 1 20 >  /dev/null
-docker exec phasedancer ./unload_index.sh flat2 1
+docker exec phasedancer ./run_phaseDancer.sh example all 1 20 >  /dev/null
+docker exec phasedancer ./unload_index.sh example 1
 docker stop phasedancer
 docker rm phasedancer
 docker run -d -it -v `pwd`/phasedancer_example:/phaseDancerViewer_data -p 8000:8000 --name phasedancer_viewer bposzewiecka/phasedancer_viewer:1.0
